1ef9d0c3027e93cd05e8d350b425dd1f57272d12
Dmitrii_Novikov1 <dmitrii_novikov1@epam.com>
2024-07-22 00:51:09 +0200

    Fixed extremes calculations

--- src/main/java/com/xm/recommendation/service/ExtremesCalculatorImpl.java
+++ src/main/java/com/xm/recommendation/service/ExtremesCalculatorImpl.java
@@ -15,32 +15,61 @@ public class ExtremesCalculatorImpl implements ExtremesCalculator {
 
   @Override
   public Map<String, ExtremesDto> calculateExtremes(List<CryptoPrice> cryptoPrices) {
+    log.debug("Calculating extremes for data source with {} crypto prices", cryptoPrices.size());
     Map<String, ExtremesDto> extremesMap = new HashMap<>();
 
     for (CryptoPrice cryptoPrice : cryptoPrices) {
       ExtremesDto extreme = extremesMap.get(cryptoPrice.symbol());
 
       if (extreme == null) {
+        log.debug(
+            "No extremes found for crypto: {}. Initialized with value: {}",
+            cryptoPrice.symbol(),
+            cryptoPrice.price());
         extremesMap.put(cryptoPrice.symbol(), ExtremesDto.fromCryptoPrice(cryptoPrice));
         continue;
       }
       if (cryptoPrice.price().compareTo(extreme.minPrice()) < 0) {
+        log.debug(
+            "New min price found for crypto: {}. New value: {}",
+            cryptoPrice.symbol(),
+            cryptoPrice.price());
         extremesMap.put(
-            cryptoPrice.symbol(), ExtremesDto.fromMinPrice(extreme, cryptoPrice.price()));
+            cryptoPrice.symbol(),
+            ExtremesDto.fromMinPrice(extremesMap.get(cryptoPrice.symbol()), cryptoPrice.price()));
       }
       if (cryptoPrice.price().compareTo(extreme.maxPrice()) > 0) {
+        log.debug(
+            "New max price found for crypto: {}. New value: {}",
+            cryptoPrice.symbol(),
+            cryptoPrice.price());
         extremesMap.put(
-            cryptoPrice.symbol(), ExtremesDto.fromMaxPrice(extreme, cryptoPrice.price()));
+            cryptoPrice.symbol(),
+            ExtremesDto.fromMaxPrice(extremesMap.get(cryptoPrice.symbol()), cryptoPrice.price()));
       }
       if (cryptoPrice.timestamp().isBefore(extreme.oldestTimestamp())) {
+        log.debug(
+            "New oldest timestamp found for crypto: {}. New value: {}",
+            cryptoPrice.symbol(),
+            cryptoPrice.timestamp());
         extremesMap.put(
             cryptoPrice.symbol(),
-            ExtremesDto.fromMinTimestamp(extreme, cryptoPrice.price(), cryptoPrice.timestamp()));
+            ExtremesDto.fromMinTimestamp(
+                extremesMap.get(cryptoPrice.symbol()),
+                cryptoPrice.price(),
+                cryptoPrice.timestamp()));
       }
       if (cryptoPrice.timestamp().isAfter(extreme.newestTimestamp())) {
+        log.debug(
+            "New newest timestamp found for crypto: {}. New value: {}",
+            cryptoPrice.symbol(),
+            cryptoPrice.timestamp());
         extremesMap.put(
             cryptoPrice.symbol(),
-            ExtremesDto.fromMaxTimestamp(extreme, cryptoPrice.price(), cryptoPrice.timestamp()));
+            ExtremesDto.fromMaxTimestamp(
+                extremesMap.get(cryptoPrice.symbol()),
+                cryptoPrice.price(),
+                cryptoPrice.timestamp()));
       }
     }
 
